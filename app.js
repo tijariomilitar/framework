68630fc31323f9eb2e086b2d06dcd4dd8442110f
tijariomilitar <tijariomilitar@gmail.com>
2020-10-15 18:00:44 -0300

    release

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'app/view'));
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+	res.render("index", { user: {name:'henrique', age: 25 }});
+});
 
 module.exports = app;
