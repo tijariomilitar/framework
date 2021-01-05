ae386d31a81111e94d91b5a4cb78d8e709734b93
tijariomilitar <tijariomilitar@gmail.com>
2021-01-05 12:52:27 -0300

    update lib.adress.fillForm

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -281,11 +281,11 @@ lib.adress = {
 	},
 	fillForm:async (cep, form) => {
 		let adress = await lib.adress.get(cep);
-		if(adress.logradouro){ document.getElementById(form).elements.namedItem("logradouro").value = adress.logradouro; };
-		if(adress.complemento){ document.getElementById(form).elements.namedItem("complemento").value = adress.complemento; };
-		if(adress.bairro){ document.getElementById(form).elements.namedItem("bairro").value = adress.bairro; };
-		if(adress.localidade){ document.getElementById(form).elements.namedItem("cidade").value = adress.localidade; };
-		if(adress.uf){ document.getElementById(form).elements.namedItem("uf").value = adress.uf; };
+		if(adress.logradouro){ document.getElementById(form).elements.namedItem("street").value = adress.logradouro; };
+		if(adress.complemento){ document.getElementById(form).elements.namedItem("complement").value = adress.complemento; };
+		if(adress.bairro){ document.getElementById(form).elements.namedItem("neighborhood").value = adress.bairro; };
+		if(adress.localidade){ document.getElementById(form).elements.namedItem("city").value = adress.localidade; };
+		if(adress.uf){ document.getElementById(form).elements.namedItem("state").value = adress.uf; };
 	}
 };
 
