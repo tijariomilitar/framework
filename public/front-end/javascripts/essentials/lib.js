88a229c8cc9d78074e0339591ca513a986b3d322
tijariomilitar <tijariomilitar@gmail.com>
2021-03-26 16:41:41 -0300

    fix timestamp to date

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -51,7 +51,7 @@ const lib = {
 		let date = new Date(parseInt(timestamp));
 		let day;let month;let hour;let minute;
 		if(date.getDate() < 10){ day = "0"+date.getDate() } else { day = date.getDate() };
-		if(date.getMonth() < 10){ month = "0"+(date.getMonth()+1) } else { month = (date.getMonth()+1) };
+		if(date.getMonth() < 10){ month = "0"+date.getMonth() } else { month = date.getMonth() };
 		if(date.getHours() < 10){ hour = "0"+date.getHours() } else { hour = date.getHours() };
 		if(date.getMinutes() < 10){ minute = "0"+date.getMinutes() } else { minute = date.getMinutes() };
 		return day+'-'+month+'-'+date.getFullYear()+' '+hour+':'+minute;
@@ -60,7 +60,7 @@ const lib = {
 		let date = new Date(parseInt(timestamp));
 		let day;let month;let hour;let minute;
 		if(date.getDate() < 10){ day = "0"+date.getDate() } else { day = date.getDate() };
-		if(date.getMonth() < 10){ month = "0"+(date.getMonth()+1) } else { month = (date.getMonth()+1) };
+		if(date.getMonth() < 10){ month = "0"+date.getMonth() } else { month = date.getMonth() };
 		if(date.getHours() < 10){ hour = "0"+date.getHours() } else { hour = date.getHours() };
 		if(date.getMinutes() < 10){ minute = "0"+date.getMinutes() } else { minute = date.getMinutes() };
 		return date.getFullYear()+'-'+month+'-'+day+'T'+hour+':'+minute;
