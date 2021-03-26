e34c8be9d3eb4fc380a8b37d2286ac209f1aff45
tijariomilitar <tijariomilitar@gmail.com>
2021-03-26 15:50:06 -0300

    fix month

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -100,7 +100,7 @@ const lib = {
         let date = new Date();
         let day;let month;let hour;let minute;
         if(date.getDate() < 10){ day = "0"+date.getDate() } else { day = date.getDate() };
-        if(date.getMonth() < 10){ month = "0"+date.getMonth()+1 } else { month = date.getMonth()+1 };
+        if(date.getMonth() < 10){ month = "0"+(date.getMonth()+1) } else { month = (date.getMonth()+1) };
         if(date.getHours() < 10){ hour = "0"+date.getHours() } else { hour = date.getHours() };
         if(date.getMinutes() < 10){ minute = "0"+date.getMinutes() } else { minute = date.getMinutes() };
         return input.value = date.getFullYear()+'-'+month+'-'+day+'T'+hour+':'+minute;
