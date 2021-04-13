d027a5900e859d9b2deab2be2b59df9422211efb
tijariomilitar <tijariomilitar@gmail.com>
2021-04-13 11:30:52 -0300

    fix timestampToDate and included timestampToFulldate

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -107,6 +107,18 @@ const lib = {
 			if(date.getMonth() < 10){ month = "0"+(date.getMonth()+1) } else { month = (date.getMonth()+1) };
 			if(date.getHours() < 10){ hour = "0"+date.getHours() } else { hour = date.getHours() };
 			if(date.getMinutes() < 10){ minute = "0"+date.getMinutes() } else { minute = date.getMinutes() };
+			return day+'-'+month+'-'+date.getFullYear();
+		};
+		return false;
+	},
+	timestampToFulldate: (timestamp) => {
+		if(timestamp){
+			let date = new Date(parseInt(timestamp));
+			let day;let month;let hour;let minute;
+			if(date.getDate() < 10){ day = "0"+date.getDate() } else { day = date.getDate() };
+			if(date.getMonth() < 10){ month = "0"+(date.getMonth()+1) } else { month = (date.getMonth()+1) };
+			if(date.getHours() < 10){ hour = "0"+date.getHours() } else { hour = date.getHours() };
+			if(date.getMinutes() < 10){ minute = "0"+date.getMinutes() } else { minute = date.getMinutes() };
 			return day+'-'+month+'-'+date.getFullYear()+' '+hour+':'+minute;
 		};
 		return false;
