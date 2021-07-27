03d52548ec889ed9321ecbccbf0c2422ade59b0b
tijariomilitar <tijariomilitar@gmail.com>
2021-07-26 23:33:52 -0300

    API.response rework

--- public/front-end/javascripts/essentials/api.js
+++ public/front-end/javascripts/essentials/api.js
@@ -10,19 +10,11 @@ const API = {
 		};
 		return false;
 	},
-	response: async (func, param, element) => {
+	response: async (func, param) => {
 		document.getElementById('ajax-loader').style.visibility = 'visible';
-		if(element){ 
-			element = await func(param);
-			document.getElementById('ajax-loader').style.visibility = 'hidden';
-			if(!element){ return false; };
-			return element;
-		} else { 
-			if(!await func(param)){ 
-				document.getElementById('ajax-loader').style.visibility = 'hidden';
-				return false 
-			};
-			return true; 
-		};
+		let element = await func(param);
+		document.getElementById('ajax-loader').style.visibility = 'hidden';
+		if(!element){ return false; }
+		return element;
 	}
 };
