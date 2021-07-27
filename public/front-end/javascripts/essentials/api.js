5830c5fde3a9897c05bd76632c92ce157b270ef3
tijariomilitar <tijariomilitar@gmail.com>
2021-07-26 22:21:26 -0300

    API.response implemented.

--- public/front-end/javascripts/essentials/api.js
+++ public/front-end/javascripts/essentials/api.js
@@ -9,5 +9,20 @@ const API = {
 			return true;
 		};
 		return false;
+	},
+	response: async (func, param, element) => {
+		document.getElementById('ajax-loader').style.visibility = 'visible';
+		if(element){ 
+			element = await func(param);
+			document.getElementById('ajax-loader').style.visibility = 'hidden';
+			if(!element){ return false; };
+			return element;
+		} else { 
+			if(!await func(param)){ 
+				document.getElementById('ajax-loader').style.visibility = 'hidden';
+				return false 
+			};
+			return true; 
+		};
 	}
 };
