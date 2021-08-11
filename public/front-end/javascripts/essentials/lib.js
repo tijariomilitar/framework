95cf6c6853165bde7c738f00f0f640a9f1a971d0
tijariomilitar <tijariomilitar@gmail.com>
2021-08-11 17:40:21 -0300

    fix image zoom

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -635,8 +635,8 @@ lib.image = {
 		let zoomer = e.currentTarget;
 		e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
 		e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
-		x = offsetX/zoomer.offsetWidth*10;
-		y = offsetY/zoomer.offsetHeight*10;
+		x = offsetX/zoomer.offsetWidth*100;
+		y = offsetY/zoomer.offsetHeight*100;
 		zoomer.style.backgroundPosition = x + '% ' + y + '%';
 	}
 };
