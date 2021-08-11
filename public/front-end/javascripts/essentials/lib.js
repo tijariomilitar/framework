8041d1f36e9e160c8cdde56cd69144a6c83ab5e3
tijariomilitar <tijariomilitar@gmail.com>
2021-08-11 14:54:21 -0300

    include image zoom

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -631,46 +631,12 @@ lib.localStorage = {
 };
 
 lib.image = {
-	zoom: (imgID, resultID) => {
-		let img, lens, result, cx, cy;
-		img = document.getElementById(imgID);
-		result = document.getElementById(resultID);
-		lens = document.createElement("DIV");
-		lens.setAttribute("class", "img-zoom-lens");
-		img.parentElement.insertBefore(lens, img);
-		cx = result.offsetWidth / lens.offsetWidth;
-		cy = result.offsetHeight / lens.offsetHeight;
-		result.style.backgroundImage = "url('" + img.src + "')";
-		result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-		lens.addEventListener("mousemove", moveLens);
-		img.addEventListener("mousemove", moveLens);
-		lens.addEventListener("touchmove", moveLens);
-		img.addEventListener("touchmove", moveLens);
-
-		function getCursorPos (e) {
-			let a, x = 0, y = 0;
-			e = e || window.event;
-			a = img.getBoundingClientRect();
-			x = e.pageX - a.left;
-			y = e.pageY - a.top;
-			x = x - window.pageXOffset;
-			y = y - window.pageYOffset;
-			return {x : x, y : y};
-		};
-
-		function moveLens (e) {
-			let pos, x, y;
-			e.preventDefault();
-			pos = getCursorPos(e);
-			x = pos.x - (lens.offsetWidth / 2);
-			y = pos.y - (lens.offsetHeight / 2);
-			if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-			if (x < 0) {x = 0;}
-			if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-			if (y < 0) {y = 0;}
-			lens.style.left = x + "px";
-			lens.style.top = y + "px";
-			result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
-		};
+	zoom: (e) => {
+		let zoomer = e.currentTarget;
+		e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
+		e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
+		x = offsetX/zoomer.offsetWidth*100;
+		y = offsetY/zoomer.offsetHeight*100;
+		zoomer.style.backgroundPosition = x + '% ' + y + '%';
 	}
 };
