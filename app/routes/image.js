8235df9e2023ac51041b193cf6a59918c3bb9d70
tijariomilitar <tijariomilitar@gmail.com>
2022-01-21 15:03:20 -0300

    remove node_modules with .gitignore and initial code to upload and compress images

--- app/routes/image.js
+++ app/routes/image.js
@@ -1,6 +1,31 @@
 const router = require("express").Router();
-// const lib = require('jarmlib');
+const multer = require('./../middleware/multer');
+const sharp = require('./../middleware/sharp');
 
 router.get("/zoom", (req, res) => { res.render("documentation/image/zoom"); });
+
+router.get("/convert", (req, res) => { res.render("documentation/image/convert"); });
+
+router.post("/convert", multer.single('image'), (req, res) => {
+    if (req.file) {
+         sharp.compressImage(req.file, 80)
+            .then(newPath => {
+                return res.send({ msg: "A imagem foi processada com sucesso!", imagePath: newPath });
+             })
+            .catch(err => {
+            	console.log(err);
+    			return res.send('Houve erro no upload!');
+            } );
+    }
+});
+
+router.get('/convert/donwload', (req, res) => {
+    console.log(req.body);
+
+    let filePath = newPath;
+    let fileName = req.file.originalname;
+
+    res.download(filePath, fileName);
+})
 
 module.exports = router;
