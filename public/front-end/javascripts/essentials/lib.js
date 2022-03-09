6fb0942757c09a6def9b26e1990aab286b7e2175
tijariomilitar <tijariomilitar@gmail.com>
2022-03-09 17:39:27 -0300

    include capitalizeFirst lib function

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -272,6 +272,14 @@ const lib = {
 		};
 		return false;
 	},
+	capitalizeFirst: (words) => {
+		let separateWord = words.toLowerCase().split(' ');
+		for (let i=0;i<separateWord.length;i++) {
+			separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+			separateWord[i].substring(1);
+		};
+		return separateWord.join(' ');
+	},
 	clearSelect(select){
 		select.innerHTML = "";
 		select.innerHTML += "<option value='0'>Sem resultados</option>"
