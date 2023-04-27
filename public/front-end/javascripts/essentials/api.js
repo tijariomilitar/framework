ed3e1ff67c8698f46c4f589b413dd526d538afd6
tijariomilitar <tijariomilitar@gmail.com>
2023-04-27 00:28:56 -0300

    update loader

--- public/front-end/javascripts/essentials/api.js
+++ public/front-end/javascripts/essentials/api.js
@@ -1,20 +1,42 @@
 const API = {
-	verifyResponse(res){
-		if(res.unauthorized){
+	verifyResponse(res) {
+		if (res.unauthorized) {
 			alert(res.unauthorized);
 			return window.location.href = '/login';
 		};
-		if(res.msg){
-			alert(res.msg);
+
+		if (res.msg) {
+			if (!document.getElementById("msg")) {
+				alert(res.msg);
+				return true;
+			}
+
+			document.getElementById("msg").style.display = "";
+			document.getElementById("msg-content").innerHTML = "";
+			document.getElementById("msg-content").append(lib.element.create("div", {
+				class: "box b1 center lucida-grande em12 bold"
+			}, res.msg));
+
 			return true;
 		};
 		return false;
 	},
+	// verifyResponse(res) {
+	// 	if (res.unauthorized) {
+	// 		alert(res.unauthorized);
+	// 		return window.location.href = '/login';
+	// 	};
+	// 	if (res.msg) {
+	// 		alert(res.msg);
+	// 		return true;
+	// 	};
+	// 	return false;
+	// },
 	response: async (func, param) => {
-		if(document.getElementById('loader')){ document.getElementById('loader').style.visibility = 'visible'; }
+		if (document.getElementById('loader')) { document.getElementById('loader').style.visibility = 'visible'; }
 		let element = await func(param);
-		if(document.getElementById('loader')){ document.getElementById('loader').style.visibility = 'hidden'; }
-		if(!element){ return false; }
+		if (document.getElementById('loader')) { document.getElementById('loader').style.visibility = 'hidden'; }
+		if (!element) { return false; }
 		return element;
 	}
 };
