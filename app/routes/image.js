3a848f9af2dea2c1e3cbb399b7cd806eb9f2fe47
tijariomilitar <tijariomilitar@gmail.com>
2023-09-15 06:41:34 -0300

    implementing new zoom system

--- app/routes/image.js
+++ app/routes/image.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const stream = require('stream');
 
 
-router.get("/zoom", (req, res) => { res.render("documentation/image/zoom"); });
+router.get("/zoom", (req, res) => { res.render("documentation/image/custom-zoom/index"); });
 
 router.get("/convert", (req, res) => { res.render("documentation/image/convert"); });
 
@@ -14,7 +14,7 @@ router.post("/convert", multer.single('image'), (req, res) => {
         sharp.compressImage(req.file)
             .then(newPath => {
                 return res.send({ done: "A imagem foi processada com sucesso!", image: req.file.filename });
-             })
+            })
             .catch(err => {
                 console.log(err);
                 return res.send({ msg: 'Houve erro no upload!' });
@@ -23,17 +23,17 @@ router.post("/convert", multer.single('image'), (req, res) => {
 });
 
 router.get('/convert/download/:url', (req, res) => {
-    let data = fs.readFileSync('public/images/download/'+ req.params.url.split('.')[0] + '.png');
+    let data = fs.readFileSync('public/images/download/' + req.params.url.split('.')[0] + '.png');
     let fileContents = Buffer.from(data, "base64");
-  
+
     let readStream = new stream.PassThrough();
     readStream.end(fileContents);
 
     res.set('Content-disposition', 'attachment; filename=' + req.params.url.split('.')[0] + '.png');
     res.set('Content-Type', 'text/plain');
 
-    fs.access('public/images/download/'+ req.params.url.split('.')[0] + '.png', (err) => {
-        if (!err) { fs.unlink('public/images/download/'+ req.params.url.split('.')[0] + '.png', err => { if(err) console.log(err) }) }
+    fs.access('public/images/download/' + req.params.url.split('.')[0] + '.png', (err) => {
+        if (!err) { fs.unlink('public/images/download/' + req.params.url.split('.')[0] + '.png', err => { if (err) console.log(err) }) }
     });
 
     readStream.pipe(res);
