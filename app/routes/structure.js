33312223038f35487fb3a489e1556149b69d42a4
tijariomilitar <tijariomilitar@gmail.com>
2023-10-24 13:52:16 -0300

    update box

--- app/routes/structure.js
+++ app/routes/structure.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/container", (req, res) => { res.render("documentation/structure/container"); });
-router.get("/box", (req, res) => { res.render("documentation/structure/box"); });
+router.get("/box", (req, res) => { res.render("documentation/structure/box/index"); });
 router.get("/position", (req, res) => { res.render("documentation/structure/position"); });
 router.get("/responsive", (req, res) => { res.render("documentation/structure/responsive"); });
 router.get("/carousel", (req, res) => { res.render("documentation/structure/carousel"); });
