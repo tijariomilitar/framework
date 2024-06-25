be7a2ab7b7acd3a949a14ecaae43ce467ea41b02
hhlyras2011@gmail.com <hhlyras2011@gmail.com>
2024-06-25 20:58:44 -0300

    update image zoom

--- public/front-end/javascripts/essentials/lib.js
+++ public/front-end/javascripts/essentials/lib.js
@@ -1561,13 +1561,111 @@ lib.localStorage.remove = (item) => {
 
 lib.image = {};
 
+// lib.popup = (element, cb) => {
+// 	const focused_btn = document.querySelector(':focus');
+// 	focused_btn && focused_btn.blur();
+
+// 	const msg_div = lib.element.create("div", {
+// 		class: "msg h-center",
+// 		style: "z-index: 10;"
+// 	});
+// 	const msg_popup = lib.element.create("div", {
+// 		class: "msg-popup box a3-4 container radius-5 scroll-y-auto scroll-small"
+// 	});
+
+// 	const close_div = lib.element.create("div", {
+// 		class: "mobile-box a1 container ground center sticky",
+// 		style: "top: 0;z-index: 10;"
+// 	});
+// 	msg_popup.append(close_div);
+// 	close_div.append(lib.element.create("div", { class: "mobile-box b9-10" }));
+// 	const close_icon = lib.element.create("div", { class: "em20 icon pointer center" }, "&times;");
+// 	close_div.append(close_icon);
+
+// 	msg_popup.append(element);
+
+// 	msg_div.append(msg_popup);
+// 	document.body.append(msg_div);
+// 	document.body.style.overflow = "hidden";
+
+// 	function esc() {
+// 		document.removeEventListener("keydown", keydown);
+// 		document.body.style.overflow = "auto";
+// 		msg_div.remove();
+// 		if (typeof cb === 'function') { return cb(); }
+// 	};
+
+// 	function keydown(e) {
+// 		if (e.keyCode == 27) { esc(); }
+// 	};
+
+// 	close_icon.addEventListener("click", esc);
+// 	document.addEventListener("keydown", keydown);
+// };
+
 lib.image.zoom = (image_src) => {
 	let image = lib.element.create("img", {
 		src: image_src,
-		class: "image-prop max-height-500 center"
+		class: "image-prop max-height-500 border center"
 	});
 
-	lib.popup(image);
+	const focused_btn = document.querySelector(':focus');
+	focused_btn && focused_btn.blur();
+
+	const msg_div = lib.element.create("div", {
+		class: "msg h-center",
+		style: "z-index: 10;"
+	});
+	const msg_popup = lib.element.create("div", {
+		class: "msg-popup box a3-4 container radius-5 scroll-y-auto scroll-x-hide scroll-small",
+		style: "position: relative;" // Adicionei position: relative para o msg_popup
+	});
+
+	const close_div = lib.element.create("div", {
+		class: "close-container",
+		style: "position: absolute; top: 5px; right: 5px; z-index: 11;" // Estilos ajustados para a posição superior direita
+	});
+	// const close_icon = lib.element.create("div", {
+	// 	class: "rem20 icon pointer center",
+	// 	style: "color: #000;text-shadow:\
+	// 			-1px -1px 0.1px #fff,\
+	//       1px -1px 0.1px #fff,\
+	//       -1px  1px 0.1px #fff,\
+	// 			1px  1px 0.1px #fff;"
+	// }, "&times;");
+
+	let close_icon = lib.element.create("div", {
+		class: "ground border-st size-20 radius-50 padding-1 absolute pointer",
+		style: "top: 5px; right: 5px;"
+	});
+	close_div.append(close_icon);
+
+	close_icon.append(lib.element.create("img", {
+		class: "size-20 opacity-out-08",
+		src: "/images/icon/close-small.png"
+	}));
+
+	msg_popup.append(close_div);
+
+	msg_popup.append(image);
+
+	msg_div.append(msg_popup);
+	document.body.append(msg_div);
+	document.body.style.overflow = "hidden";
+
+	function esc() {
+		document.removeEventListener("keydown", keydown);
+		document.body.style.overflow = "auto";
+		msg_div.remove();
+		if (typeof cb === 'function') { return cb(); }
+	};
+
+	function keydown(e) {
+		if (e.keyCode == 27) { esc(); }
+	};
+
+	close_icon.addEventListener("click", esc);
+	document.addEventListener("keydown", keydown);
 };
 
 lib.image.carousel = (images, parentElement, cb) => {
